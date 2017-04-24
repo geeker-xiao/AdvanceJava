1965464f5722bf5e275ad8ad806d426e12aa4575
xiaoran <414153025@qq.com>
2017-04-24 13:18:19 +0800

    试试,不然会错误的

---
+++ src/main/java/com/beans/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.beans.test;
+
+/**
+ * Created by sunii on 2017.4.24.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
