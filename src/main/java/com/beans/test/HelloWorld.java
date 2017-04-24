fc873c6b301622002b25909c548e18dd0b121d3a
VIP萧然 <414153025@qq.com>
2017-04-24 13:21:45 +0800

    更新 HelloWorld.java

--- src/main/java/com/beans/test/HelloWorld.java
+++ src/main/java/com/beans/test/HelloWorld.java
@@ -5,6 +5,7 @@ package com.beans.test;
  */
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("这是测试的,不要当真");
         System.out.println("helloworld");
     }
 }
